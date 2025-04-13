6d8cf4e864b580502b57ec2f1d1153130b311d87
Juan Gómez <juangomez.profesional@gmail.com>
2025-04-13 19:05:00 +0100

    Added guitar to header and adapted desktop styles

--- src/components/Header/PageHeader.vue
+++ src/components/Header/PageHeader.vue
@@ -6,23 +6,26 @@ import SvgIcon from "@shared/ui/Icon/SvgIcon.vue";
 
 <template>
   <header class="header">
-    <div class="header__nav">
-      <a href="/">
-        <img class="header__nav__logo" src="/public/icons/logo.svg" type="image/svg+xml" alt="">
-      </a>
+    <div class="header__container">
+      <div class="header__nav">
+        <a href="/">
+          <img class="header__nav__logo" src="/public/icons/logo.svg" type="image/svg+xml" alt="">
+        </a>
+      </div>
+      <div class="header__content">
+        <Title :level="1" :cssClass="'title--primary title--big'">Guitar course</Title>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, possimus quibusdam dolor nemo velit quo,
+          fuga omnis, iure molestias optio tempore sint at ipsa dolorum odio exercitationem eos inventore odit.
+        </p>
+        <Title :level="2" :cssClass="'title--primary title--big'">$399</Title>
+        <Button href="tel:+34000000000" class-list="button--primary">
+          <SvgIcon name="contact" alt="Contact now icon" />
+          <span>Contact now</span>
+        </Button>
+      </div>
     </div>
-    <div class="header__content">
-      <Title :level="1" :cssClass="'title--primary title--big'">Guitar course</Title>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, possimus quibusdam dolor nemo velit quo,
-        fuga omnis, iure molestias optio tempore sint at ipsa dolorum odio exercitationem eos inventore odit.
-      </p>
-      <Title :level="2" :cssClass="'title--primary title--big'">$399</Title>
-      <Button href="tel:+34000000000" class-list="button--primary">
-        <SvgIcon name="contact" alt="Contact now icon" />
-        <span>Contact now</span>
-      </Button>
-    </div>
+    <img class="header__guitar" src="/public/img/header_guitar.png" alt=""></img>
   </header>
 </template>
 
