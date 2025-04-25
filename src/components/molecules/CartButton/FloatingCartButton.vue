9fcd37b3a905d935df0fbef663c3d06cff47ad34
Juan Gómez <juangomez.profesional@gmail.com>
2025-04-25 15:15:23 +0100

    Removed props from the floating cart button and renamed correctly

---
+++ src/components/molecules/CartButton/FloatingCartButton.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import SvgIcon from "@components/atoms/Icon/SvgIcon.vue";
+import Button from "@components/atoms/Button/Button.vue";
+import {useCartStore} from "@stores/cart.store.ts";
+
+const cartStore = useCartStore();
+
+</script>
+
+<template>
+  <Button :on-click="cartStore.toggleCart" v-if="cartStore.totalItems > 0" class="fixed fixed--right fixed--bottom"
+          class-list="button--primary button--circle">
+    <SvgIcon class="icon--no-margin" name="cart" alt="Cart icon" />
+    <span class="badge">{{ cartStore.totalItems }}</span>
+  </Button>
+</template>
+
+<style scoped lang="scss" src="./FloatingCartButton.scss" />
